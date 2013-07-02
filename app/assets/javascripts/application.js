88971b9f51b8182f74eb710212a7a7cad3156e2c
Marina P <petrova.dll@gmail.com>
2013-07-02 10:33:01 +0200

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,54 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+var socket;
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function animateCharacter(letter)
+{
+  var upper = letter.toUpperCase();
+  $('#character_' + upper)
+    .stop(true,true)
+    .animate({ opacity: 1}, 100)
+    .animate({ opacity: .2}, 100);
+}
+
+function setup()
+{
+  var target = $('#alphabet');
+  for(var i = 0; i <=alphabet.length; i++)
+  {
+    var char = alphabet.charAt(i);
+    target.append('<span id="character_' + char +'">' + char +
+        '</span');
+  }
+  connect();
+
+  $(document).keypress(function(e){
+    var char = String.fromCharCode(e.keyCode);
+    socket.send(char);
+  });
+};
+
+function connect(){
+  socket = new WebSocket('ws://127.0.0.1:8080');
+  socket.onmessage = function(mess) {
+    animateCharacter(mess.data);
+  };
+
+};
+
+window.onload += setup();
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Websockets::Application.config.secret_key_base = 'ef87611e84a4a657f32265a2e22db9bcd81081536e4a09b41cf46a0921556cb5650add83403ccc6050e4cf75b8aa5c1d1548559e238dd508741a4e64aadd6e13'
