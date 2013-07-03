1f0c3479e25bee3867eaec30f592fe67f1f8621e
Marina P <petrova.dll@gmail.com>
2013-07-03 23:00:25 +0200

    add multiuser ability

--- app/ws.rb
+++ app/ws.rb
@@ -1,15 +1,18 @@
 require 'websocket-eventmachine-server'
 EM.run do
-
+  @sockets = []
   WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080) do
     |ws|
     ws.onopen do
+      @sockets << ws
       puts "Client connected"
     end
 
     ws.onmessage do |msg, type|
-      puts "Received message: #{msg}"
-      ws.send msg, :type => type
+      puts "Received message: #{msg} #{type}"
+      @sockets.each do |sk|
+        sk.send msg, :type => type
+      end
     end
 
     ws.onclose do
